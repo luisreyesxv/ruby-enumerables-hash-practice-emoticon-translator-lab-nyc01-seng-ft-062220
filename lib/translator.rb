5bdac49c4c6318fbc97464a5028f67f4b067b3d4
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-15 22:27:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(emoticon_library,english_emoticon)
   answer = load_library(emoticon_library)
   #answer.find{|meaning_key,language_hash| language_hash[:english]==english_emoticon}[1][:japanese]
 answer[get_english_meaning(emoticon_library,english_emoticon)]
-s
+
 
 
 end
