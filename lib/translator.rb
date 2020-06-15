17dffbf7fcc78a24e1ad9ad3c2ef04a79a618573
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-15 22:38:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,12 @@ def get_japanese_emoticon(emoticon_library,english_emoticon)
 
   meaning = load_library(emoticon_library)
   #answer.find{|meaning_key,language_hash| language_hash[:english]==english_emoticon}[1][:japanese]
-meaning[get_english_meaning(emoticon_library,english_emoticon)].fetch(:japanese,"Sorry, that emoticon was not found")
+  if !meaning[get_english_meaning(emoticon_library,english_emoticon)]
+    return "Sorry, that emoticon was not found"
+  else
+    meaning[get_english_meaning(emoticon_library,english_emoticon)][:japanese]  
+  end  
+    
 
 
 
