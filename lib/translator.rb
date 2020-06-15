d363e9c7d470979555cff6f97c21a402ae34aac9
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-15 22:00:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
 # require modules here
-
-def load_library
-  # code goes here
+require 'pry'
+require 'yaml'
+def load_library(library)
+  beginning = YAML.load_file(library)
+  beginning.each_with_object({}) do |(meaning_key,emoticon_array),fixed_array|
+      if !fixed_array[meaning_key]
+        fixed_array[meaning_key]={}
+      end
+      if !fixed_array[meaning_key][:english]
+        fixed_array[meaning_key][:english] = emoticon_array[0]
+      end
+      if !fixed_array[meaning_key][:japanese]
+        fixed_array[meaning_key][:japanese] = emoticon_array[1]
+      end
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticon_library,english_emoticon)
+
+  answer = load_library(emoticon_library)
+  answer.find{|meaning_key,language_hash| language_hash[:english]==english_emoticon}[:japanese]
+
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(emoticon_library,emoticon)
+  library = load_library(emoticon_library)
+  test =library.find{|meaning_key,language_hash|  language_hash[:japanese]==emoticon || language_hash[:english]== emoticon}
+
+  if test == nil
+    return "Sorry, that emoticon was not found"
+  else
+    test[0]
+  end
 end
